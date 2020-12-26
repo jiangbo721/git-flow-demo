ef409eb4c3144d0a529843784634288ffb8672e6
ljb5 <ljb5@meitu.com>
2020-12-26 16:21:40 +0800

    develop: demo commit

---
+++ demo.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*
+
+import time
+
+
+def fib(n):
+    if n <= 2:
+        res = 1
+    else:
+        res = fib(n - 1) + fib(n - 2)
+
+    return res
+
+
+def sum_squares(n):
+    start_time = time.time()
+    res = 0
+    for i in range(n + 1):
+        res += i ** 2
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f'计算0到{n}的平方和耗时:{duration: .4f}s, 将结果为:{res}')
+    return res
+
+
+if __name__ == '__main__':
+    sum_squares(10000)
